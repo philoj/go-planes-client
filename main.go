6c1a9866a482b11c14fb10a2ac4819dfd4487677
philoj <philoj@qburst.com>
2020-04-07 16:50:17 +0530

    start monitoring keypresses; updated player icon image

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"flag"
 	"github.com/hajimehoshi/ebiten"
 	"log"
-	"os"
-	"runtime/pprof"
 	"watchYourSix/planes"
 )
 
-var cpuProfile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
-	flag.Parse()
-	if *cpuProfile != "" {
-		f, err := os.Create(*cpuProfile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal(err)
-		}
-		defer pprof.StopCPUProfile()
-	}
 	ebiten.SetWindowSize(planes.ScreenWidth*2, planes.ScreenHeight*2)
 	ebiten.SetWindowTitle("Watch you Six")
 	if err := ebiten.RunGame(planes.NewGame()); err != nil {
--- planes/game.go
+++ planes/game.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	_ "image/jpeg"
+	_ "image/jpeg" // required for the image file loading to work. see ebitenutil.NewImageFromFile
+	_ "image/png"
 	"math"
 )
 
@@ -25,14 +26,14 @@ const (
 	playerIconScaleX = 0.05
 	playerIconScaleY = 0.05
 
-	initialVelocity = 2.0
+	initialVelocity = 0.5
 )
 
 var bgLayer, playerIcon *ebiten.Image
 
 func init() {
 	tile, _, _ := ebitenutil.NewImageFromFile("planes/assets/bg.jpg", ebiten.FilterDefault)
-	player, _, _ := ebitenutil.NewImageFromFile("planes/assets/icon.jpg", ebiten.FilterDefault)
+	player, _, _ := ebitenutil.NewImageFromFile("planes/assets/icon_orig.png", ebiten.FilterDefault)
 	playerIcon, _ = ebiten.NewImage(32, 32, ebiten.FilterDefault)
 	pTran := ebiten.ScaleGeo(playerIconScaleX, playerIconScaleY)
 	playerIcon.DrawImage(player, &ebiten.DrawImageOptions{
@@ -69,30 +70,33 @@ func init() {
 
 type Game struct {
 	x              float64
+	dx             float64
 	y              float64
+	dy             float64
 	bgLayerOffsetY float64
 }
 
 func NewGame() *Game {
 	return &Game{
 		bgLayerOffsetY: 0.0,
+		dx:             0.0,
+		dy:             initialVelocity,
 	}
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
-	dx, dy := 0.0, initialVelocity
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) && ebiten.IsKeyPressed(ebiten.KeyRight) {
-		fmt.Println("both")
-	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) && !ebiten.IsKeyPressed(ebiten.KeyRight) {
 		fmt.Println("left")
-		dx -= 0.1 * initialVelocity
-		dy -= initialVelocity/4
-	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.dx -= 0.1 * initialVelocity
+		g.dy -= 0.1 * initialVelocity
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) && !ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		fmt.Println("Right")
-		dx += 0.1 * initialVelocity
-		dy -= initialVelocity/4
+		g.dx += 0.1 * initialVelocity
+		g.dy -= 0.1 * initialVelocity
+	} else {
+
 	}
-	g.move(screen, dx, dy)
+	g.move(screen, g.dx, g.dy)
 	return nil
 }
 
@@ -114,7 +118,7 @@ func (g *Game) move(screen *ebiten.Image, dx float64, dy float64) {
 		SourceRect:    nil,
 	})
 
-	iconTran := ebiten.TranslateGeo(ScreenWidth/2 - 16, ScreenHeight-100)
+	iconTran := ebiten.TranslateGeo(ScreenWidth/2-16, ScreenHeight-100)
 
 	screen.DrawImage(playerIcon, &ebiten.DrawImageOptions{
 		GeoM:          iconTran,
@@ -126,8 +130,8 @@ func (g *Game) move(screen *ebiten.Image, dx float64, dy float64) {
 		SourceRect:    nil,
 	})
 	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%f", g.y), 0, 0)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%f", bgOffsetX), 100, 10)
-	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%f", bgOffsetY), 0, 10)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%f", g.dx), 100, 10)
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%f", g.dy), 0, 10)
 }
 
 func (g Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
